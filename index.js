36d43d29205c2fd3698a3297516067a52870d857
batunii <sy.soni@live.com.sg>
2024-02-18 01:23:06 +0530

    Morgen Integration

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ exports.handler = async (event) => {
     auth: process.env.NOTION_API_KEY,
   });
   const projectDatabseId = process.env.NOTION_DATABASE_ID;
-  let notion_proj_id = process.env.TICK_TICK_PROJECT_ID
+  const notion_proj_id = process.env.TICK_TICK_PROJECT_ID;
+  const morgenAPIId = process.env.MORGEN_API_ID;
+  const accountId = process.env.ACCOUNT_ID;
+  const calendarId = process.env.CALENDAR_ID;
 
-  let auth_headers = {
+
+  const auth_headers = {
     authorization: process.env.TICK_TICK_AUTH_CODE
-  }
+  };
   let tasks = [];
+  let events = [];
 
   // Getting today's and tomorrow's date
   let today = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }));
@@ -62,14 +67,30 @@ exports.handler = async (event) => {
     results.forEach(result => {
       console.log("Name of the Task ", result.properties.Tasks.title[0].plain_text);
       console.log("Day this task was created ", result.properties['Created time'].created_time);
+      console.log("Start Date of the task is: ", result.properties['Start Date'].date?.start);
       let task = {
-        title: result.properties.Tasks.title[0].plain_text + " Day " + differenceOfDays(new Date(result.properties['Created time'].created_time), today),
+        title: result.properties['Start Date'].date?.start ? result.properties.Tasks.title[0].plain_text + " Day " + differenceOfDays(new Date(result.properties['Start Date'].date.start), today)
+          : result.properties.Tasks.title[0].plain_text + " Day " + differenceOfDays(new Date(result.properties['Created time'].created_time), today),
         projectId: notion_proj_id,
         dueDate: result.properties['Due Date'].date?.start ? `${result.properties['Due Date'].date?.start}T00:01:00+0530` :
           `${today.toISOString().slice(0, 10)}T23:59:59+0530`,
       };
       console.log(task);
       tasks.push(task);
+      if (result.properties['Morgen Reminder'].select?.name) {
+        events.push({
+          accountId: accountId,
+          calendarId: calendarId,
+          title: task.title,
+          start: `${today.toISOString().slice(0, 10)}T${result.properties['Morgen Reminder'].select?.name}:00`,
+          duration: "PT120M",
+          timeZone: "Asia/Kolkata",
+          showWithoutTime: false,
+          privacy: "public",
+          freeBusyStatus: "busy"
+        });
+
+      }
     }
     )
   }).catch(error => console.log(error));
@@ -81,9 +102,25 @@ exports.handler = async (event) => {
     console.log(resp.data);
 
   };
+  async function postEvent(event) {
+    let resp = await axios.post("https://api.morgen.so/v3/events/create", event,
+      {
+        headers: {
+          "accept": "application/json",
+          "Authorization": `ApiKey ${morgenAPIId}`,
+        }
+      })
+
+    console.log(resp.data);
+  }
+
   for (let i = 0; i < tasks.length; i++) {
     await postTask(tasks[i]);
     console.log("Post done for ", tasks[i].title)
+  }
+  for (let i = 0; i < events.length; i++) {
+    await postEvent(events[i]);
+    console.log("Event done for ", events[i].title)
   }
   /**
     * @param date1 date2
@@ -92,8 +129,11 @@ exports.handler = async (event) => {
     */
 
   function differenceOfDays(date1, date2) {
+    date2.setTime(date2.getTime() + (330 * 60 * 1000));
+    console.log("Date 1 is: ", date1.toISOString());
+    console.log("Date 2 is: ", date2.toISOString());
     const oneDay = 1000 * 60 * 60 * 24;
     const difference = Math.abs(date1 - date2);
-    return Math.round(difference / oneDay);
+    return Math.ceil(difference / oneDay);
   }
 }
